4a24d73da1b43619b399c0afe100d60116b4d678
Rodolfo Granata <warlock.cc@gmail.com>
2011-11-15 00:04:12 -0300

    * foreach_cluster: apply callback to a clustered group

--- functions.bash
+++ functions.bash
@@ -48,3 +48,31 @@ function file_data_age {
     echo $f:$(( ($(date +%s) - $(stat -c %Y "$f")) / 86400 ))
   done
 }
+
+function foreach_cluster {
+
+  if [ $# -lt 3 ]; then
+    echo "usage: $0 <rgx-to-make-key> <callback> file [ file ... ]"
+    return 255
+  fi
+
+  local keymaker="$1"; shift;
+  local callback="$1"; shift;
+  local files="$@"
+
+  local _keys=""
+  for f in $files; do
+    local _key=$(basename $f | sed -e "$keymaker")
+    # _idx is the key slightly modified to comply to variable names
+    local _idx=$(echo _cluster_${_key} | sed 's:[^0-9a-zA-Z_]:_:g')
+    # register new key (no associative arrays yet :-(
+    [[ "${_keys}" =~ $_idx ]] || _keys="${_keys} $_idx"
+    # add the entry to the cluster
+    eval local ${_idx}=\"\${!_idx} $f\"
+  done
+
+  for key in ${_keys}; do
+    #echo ${key#_cluster_}: >&2
+    $callback ${!key}
+  done
+}
