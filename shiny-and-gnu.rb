d69b524c8f84463211b26f2cac0c2b6ae5088dd8
Rodolfo Granata <warlock.cc@gmail.com>
2015-03-18 09:36:59 +0000

    tune

--- shiny-and-gnu.rb
+++ shiny-and-gnu.rb
@@ -33,34 +33,12 @@ class ShinyAndGnu < Formula
   depends_on 'python' => '--with-brewed-openssl'
   depends_on 'homebrew/dupes/rsync'
   depends_on 'vim' => '--override-system-vi'
-=begin
-  depends_on 'macvim'
-  depends_on 'zsh'
-=end
 
-=begin
-  # It's complicated...
-  option 'with-gdb', 'If you want to replace `gdb`, see `brew info gdb`'
-  depends_on 'gdb' if build.with? 'gdb'
-
-  # May cause linking errors. Use with caution!
-  option 'with-m4', 'Known to cause problems, see `brew info m4`'
-  depends_on 'm4' if build.with? 'm4'
-
-  option 'with-make', 'Known to cause problems, see `brew info make`'
-  depends_on 'make' if build.with? 'make'
-
-  option 'with-unzip', 'Known to cause problems, see `brew info unzip`'
-  depends_on 'unzip'
-=end
+  # Misc
+  depends_on "mosh"
+  depends_on "weechat"
 
   def install
     opoo 'Enjoy your GNU environment!'
   end
-
-=begin
-  test do ## TODO!
-    opoo 'Maybe one day this will do something...'
-  end
-=end
 end
