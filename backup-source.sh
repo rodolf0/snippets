e2fe07239d7c9c238f22e59f85420e8b28ef017a
Rodolfo Granata <warlock.cc@gmail.com>
2018-01-30 23:03:27 -0500

    tune

--- backup-source.sh
+++ backup-source.sh
@@ -4,6 +4,7 @@ set -e -o pipefail
 backup_file=~/Source/archive/Source-$(date +%F_%T).tbz2
 tar jcf "${backup_file}.tmp" \
     --exclude=Source/archive \
+    --exclude=\*/node_modules \
     --exclude=\*/target/{debug,release} \
     ~/Source/ 2>/dev/null &&
   mv "${backup_file}.tmp" "$backup_file"
